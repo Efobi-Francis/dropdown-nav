92a2a83673e4857a43543ac43c14ecbaf84bcb35
f0b <francisefobi@gmail.com>
2023-09-07 17:35:20 +0100

    tsx solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navigation from './components/navigation'
 import Hero from './components/hero'
 import mobilehero from './assets/images/image-hero-mobile.png'
